e62551840e27e30abe5202fd34a7831ec74406af
SandgroperSoftware <sandgropersoftware@protonmail.com>
2021-07-12 14:30:35 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var menubutton = document.querySelector('.menu-button');
+menubutton.onclick = function(e)
+{
+    e.preventDefault();
+    document.querySelector('.radial-menu').classList.toggle('open');
+}
+
+var items = document.querySelectorAll('.radial-menu a');
+for(var i = 0, l = items.length; i < l; i++)
+{
+    items[i].style.left = (50 - 35*Math.cos(-0.5 * Math.PI - 2 * (1 / l) * i * Math.PI)).toFixed(4) + "%";
+    items[i].style.top = (50 + 35 * Math.sin(-0.5 * Math.PI - 2 * (1/l) * i * Math.PI)).toFixed(4) + "%";
+}
